cb1faa6fb6a7306eba2d883793cdc1419009e396
hemonugi <hemonugi@gmail.com>
2023-06-10 17:37:30 +0400

    Добавлен swagger

--- src/Controller/ApplicationController.php
+++ src/Controller/ApplicationController.php
@@ -8,10 +8,14 @@ use DateTime;
 use Hemonugi\ToolKitTestAssignment\Domain\Application\ApplicationRepositoryInterface;
 use Hemonugi\ToolKitTestAssignment\Domain\Application\GetListAction;
 use Hemonugi\ToolKitTestAssignment\Domain\Application\GetListDto;
+use Hemonugi\ToolKitTestAssignment\Domain\Application\ValidationException;
+use Hemonugi\ToolKitTestAssignment\Domain\Application\ViewDto;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Attributes as OA;
 
 /**
  * Обработчики запросов для работы с заявками
@@ -19,13 +23,45 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/api/application')]
 class ApplicationController extends AbstractController
 {
+    /**
+     * @param Request $request
+     * @param GetListAction $getListAction
+     * @param ApplicationRepositoryInterface $repository
+     * @return JsonResponse
+     * @throws ValidationException
+     */
     #[Route('/list', name: 'application_list', methods: ['get'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Список заявок',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: ViewDto::class))
+        )
+    )]
+    #[OA\Parameter(
+        name: 'statuses[]',
+        description: 'Список статусов',
+        in: 'query',
+        schema: new OA\Schema(type: 'array', items: new OA\Items(type: 'string'))
+    )]
+    #[OA\Parameter(
+        name: 'startDate',
+        description: 'Заявки после этой даты будут попадать в выборку',
+        in: 'query',
+        schema: new OA\Schema(type: 'date')
+    )]
+    #[OA\Parameter(
+        name: 'endDate',
+        description: 'Заявки до этой даты будут попадать в выборку',
+        in: 'query',
+        schema: new OA\Schema(type: 'date')
+    )]
     public function index(
         Request $request,
         GetListAction $getListAction,
         ApplicationRepositoryInterface $repository,
     ): JsonResponse {
-
         $startDate = $request->query->get('startDate');
         $endDate = $request->query->get('endDate');
 
--- src/Domain/Application/ViewDto.php
+++ src/Domain/Application/ViewDto.php
@@ -6,17 +6,29 @@ namespace Hemonugi\ToolKitTestAssignment\Domain\Application;
 
 use DateTimeInterface;
 use JsonSerializable;
+use OpenApi\Attributes as OA;
 
 /**
  * Представление данных заявки
  */
+#[OA\Schema(
+    title: "ApplicationView",
+    properties:
+        [
+            new OA\Property(property: 'id', description: 'Идентификатор'),
+            new OA\Property(property: 'title', description: 'Заголовок'),
+            new OA\Property(property: 'text', description: 'Описание'),
+            new OA\Property(property: 'createDate', description: 'Дата создания заявки'),
+            new OA\Property(property: 'status', description: 'Статус'),
+        ]
+)]
 readonly final class ViewDto implements JsonSerializable
 {
     public function __construct(
         public int $id,
         public string $title,
         public string $text,
-        public DateTimeInterface $dateTime,
+        public DateTimeInterface $createDate,
         public string $status
     ) {
     }
@@ -27,7 +39,7 @@ readonly final class ViewDto implements JsonSerializable
             'id' => $this->id,
             'title' => $this->title,
             'text' => $this->text,
-            'createDate' => $this->dateTime->format('Y-m-d H:i:s'),
+            'createDate' => $this->createDate->format('Y-m-d H:i:s'),
             'status' => $this->status,
         ];
     }
